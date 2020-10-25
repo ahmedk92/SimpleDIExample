ac3afbc2c40444506434041ba66f3b1486713a93
Ahmed Khalaf <ahmedkhalaf.92@gmail.com>
2020-10-25 18:40:16 +0200

    Initial Commit

---
+++ DIExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DIExample
+//
+//  Created by Ahmed Khalaf on 10/25/20.
+//  Copyright © 2020 Ahmed Khalaf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DIExampleTests/DIExampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  DIExampleTests.swift
+//  DIExampleTests
+//
+//  Created by Ahmed Khalaf on 10/25/20.
+//  Copyright © 2020 Ahmed Khalaf. All rights reserved.
+//
+
+import XCTest
+@testable import DIExample
+
+class DIExampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
